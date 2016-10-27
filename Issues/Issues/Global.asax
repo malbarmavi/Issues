27244383b793661c1d61e8bdfaea08a6f81b83d6
Muhammad Albarmawi <MuhammadAlbarmawi@outlook.com>
2016-10-27 07:10:40 +0200

    Add asp.net mvc default project

---
+++ Issues/Issues/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Issues.MvcApplication" Language="C#" %>
---
+++ Issues/Issues/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Issues.Startup))]
+namespace Issues
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
