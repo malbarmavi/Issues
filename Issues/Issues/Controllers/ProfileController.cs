a81fd7a4149fd8f01d70b821ab525332e01dc482
mbarmawi <muhammadbarmawi@outlook.com>
2016-11-09 09:45:58 +0300

    Add profile controller and handle read,edit opperation

---
+++ Issues/Issues/Controllers/ProfileController.cs
@@ -0,0 +1,60 @@
+﻿using Issues.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Issues.Controllers {
+    public class ProfileController : Controller {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [HttpGet]
+        public async Task<ActionResult> Index() {
+
+            string currentUserId = User.Identity.GetUserId();
+            Profile currentUserProfile = await db.Profile.FindAsync(currentUserId);
+
+            return View(currentUserProfile);
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult> Edit() {
+
+            string currentUserId = User.Identity.GetUserId();
+            Profile currentUserProfile = await db.Profile.FindAsync(currentUserId);
+
+            return View(currentUserProfile);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(Profile model) {
+
+            if (ModelState.IsValid) {
+
+                string currentUserId = User.Identity.GetUserId();
+                Profile currentUserProfile = await db.Profile.FindAsync(currentUserId);
+
+                currentUserProfile.DateOfUpdate = DateTime.Now;
+                currentUserProfile.Address = model.Address;
+                currentUserProfile.FirstName = model.FirstName;
+                currentUserProfile.LastName = model.LastName;
+                currentUserProfile.PhoneNumber = model.PhoneNumber;
+                currentUserProfile.Job = model.Job;
+                currentUserProfile.Gender = model.Gender;
+
+                db.Entry(currentUserProfile).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+
+               return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+    }
+}
