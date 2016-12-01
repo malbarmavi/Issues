e32313be6e13008c3c99a561f7b2010fb8e6f6e6
mbarmawi <muhammadbarmawi@outlook.com>
2016-12-01 06:57:22 +0300

    Add utility , app setting class

---
+++ Issues/Issues/App_Code/AppSetting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Issues {
+    public static class AppSetting {
+
+        public static int Build { get; set; } = 1;
+    }
+}
---
+++ Issues/Issues/App_Code/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Issues {
+    public static class Utility {
+
+        /// <summary>
+        /// Force the browser to remove the cash file after update by generate 
+        /// new url end with build number
+        /// </summary>
+        /// <param name="url">Url Address</param>
+        /// <returns>Url with build number</returns>
+        public static string UrlBuilder(string url) => $"{url}?v={AppSetting.Build}";
+
+    }
+}
