b1138e7d1c506aed7ab356723014507d05224ecd
mbarmawi <muhammadbarmawi@outlook.com>
2016-12-07 09:34:17 +0300

    Update nice scroll option to stop blinking

--- Issues/Issues/Scripts/app.js
+++ Issues/Issues/Scripts/app.js
@@ -1,19 +1,11 @@
 ﻿
 $(function () {
-    $('html').niceScroll({
+    $('body').niceScroll({
         cursorcolor: "#666",
         cursorborder: "0px solid #fff",
-        railpadding: {
-            top: 0,
-            right: 0,
-            left: 0,
-            bottom: 0
-        },
+
         cursorwidth: "8px",
         cursorborderradius: "10px",
-        cursoropacitymin: 0.8,
-        cursoropacitymax: 0.8,
-        boxzoom: true,
         horizrailenabled: false,
         zindex: 1050
     });
