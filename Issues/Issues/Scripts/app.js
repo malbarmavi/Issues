e7ec6b1c81f7d8a5ce04505af18ad872bc353ea8
mbarmawi <muhammadbarmawi@outlook.com>
2016-12-07 08:31:21 +0300

    Add nice scroll plugin

---
+++ Issues/Issues/Scripts/app.js
@@ -0,0 +1,20 @@
+﻿
+$(function () {
+    $('html').niceScroll({
+        cursorcolor: "#000",
+        cursorborder: "0px solid #fff",
+        railpadding: {
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0
+        },
+        cursorwidth: "10px",
+        cursorborderradius: "0px",
+        cursoropacitymin: 0.2,
+        cursoropacitymax: 0.8,
+        boxzoom: true,
+        horizrailenabled: false,
+        zindex: 9999
+    });
+});
