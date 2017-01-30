6c0c659eb65bff644b75b75e4c2605bc52e30d6e
mbarmawi <muhammadalbarmawi@outlook.com>
2017-01-30 09:07:48 +0300

    Add project model migration

---
+++ Issues/Issues/Migrations/201701300539479_AddProjectModel.cs
@@ -0,0 +1,41 @@
+namespace Issues.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddProjectModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Projects",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 100),
+                        Description = c.String(nullable: false, maxLength: 250),
+                        Version = c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"),
+                        DateOfCreate = c.DateTime(nullable: false),
+                        DateOfUpdate = c.DateTime(nullable: false),
+                        CompanyId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Companies", t => t.CompanyId, cascadeDelete: true)
+                .Index(t => t.CompanyId);
+            
+            AddColumn("dbo.Tasks", "Project_Id", c => c.Guid());
+            CreateIndex("dbo.Tasks", "Project_Id");
+            AddForeignKey("dbo.Tasks", "Project_Id", "dbo.Projects", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Tasks", "Project_Id", "dbo.Projects");
+            DropForeignKey("dbo.Projects", "CompanyId", "dbo.Companies");
+            DropIndex("dbo.Projects", new[] { "CompanyId" });
+            DropIndex("dbo.Tasks", new[] { "Project_Id" });
+            DropColumn("dbo.Tasks", "Project_Id");
+            DropTable("dbo.Projects");
+        }
+    }
+}
