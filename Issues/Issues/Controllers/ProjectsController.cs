a993ff90f24b48e7acc21fcfff8da718d40ee1f5
mbarmawi <muhammadalbarmawi@outlook.com>
2017-02-20 01:23:04 +0300

    Update projects controller

--- Issues/Issues/Controllers/ProjectsController.cs
+++ Issues/Issues/Controllers/ProjectsController.cs
@@ -1,130 +1,133 @@
-﻿using Issues.Models;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.Entity;
-using System.Net;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
+using System.Web;
 using System.Web.Mvc;
+using Issues.Models;
 
 namespace Issues.Controllers
 {
-  public class ProjectsController : Controller
-  {
-    private ApplicationDbContext db = new ApplicationDbContext();
-
-    // GET: Projects
-    public async Task<ActionResult> Index()
+    public class ProjectsController : Controller
     {
-      var projects = db.Project.Include(p => p.Company);
-      return View(await projects.ToListAsync());
-    }
+        private ApplicationDbContext db = new ApplicationDbContext();
 
-    // GET: Projects/Details/5
-    public async Task<ActionResult> Details(Guid? id)
-    {
-      if (id == null)
-      {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-      }
-      Project project = await db.Project.FindAsync(id);
-      if (project == null)
-      {
-        return HttpNotFound();
-      }
-      return View(project);
-    }
+        // GET: Projects
+        public async Task<ActionResult> Index()
+        {
+            var project = db.Project.Include(p => p.Company);
+            return View(await project.ToListAsync());
+        }
 
-    // GET: Projects/Create
-    public ActionResult Create()
-    {
-      ViewBag.CompanyId = new SelectList(db.Company, "Id", "Name");
-      return View();
-    }
+        // GET: Projects/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = await db.Project.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
 
-    // POST: Projects/Create
-    // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<ActionResult> Create([Bind(Include = "Id,Name,Description,Version,DateOfCreate,DateOfUpdate,CompanyId")] Project project)
-    {
-      if (ModelState.IsValid)
-      {
-        db.Project.Add(project);
-        await db.SaveChangesAsync();
-        return RedirectToAction("Index");
-      }
+        // GET: Projects/Create
+        public ActionResult Create()
+        {
+            ViewBag.CompanyId = new SelectList(db.Company, "Id", "Name");
+            return View();
+        }
 
-      ViewBag.CompanyId = new SelectList(db.Company, "Id", "Name", project.CompanyId);
-      return View(project);
-    }
+        // POST: Projects/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Description,Version,DateOfCreate,DateOfUpdate,CompanyId")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Project.Add(project);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
 
-    // GET: Projects/Edit/5
-    public async Task<ActionResult> Edit(int? id)
-    {
-      if (id == null)
-      {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-      }
-      Project project = await db.Project.FindAsync(id);
-      if (project == null)
-      {
-        return HttpNotFound();
-      }
-      ViewBag.CompanyId = new SelectList(db.Company, "Id", "Name", project.CompanyId);
-      return View(project);
-    }
+            ViewBag.CompanyId = new SelectList(db.Company, "Id", "Name", project.CompanyId);
+            return View(project);
+        }
 
-    // POST: Projects/Edit/5
-    // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Description,Version,DateOfCreate,DateOfUpdate,CompanyId")] Project project)
-    {
-      if (ModelState.IsValid)
-      {
-        db.Entry(project).State = EntityState.Modified;
-        await db.SaveChangesAsync();
-        return RedirectToAction("Index");
-      }
-      ViewBag.CompanyId = new SelectList(db.Company, "Id", "Name", project.CompanyId);
-      return View(project);
-    }
+        // GET: Projects/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = await db.Project.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CompanyId = new SelectList(db.Company, "Id", "Name", project.CompanyId);
+            return View(project);
+        }
 
-    // GET: Projects/Delete/5
-    public async Task<ActionResult> Delete(Guid? id)
-    {
-      if (id == null)
-      {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-      }
-      Project project = await db.Project.FindAsync(id);
-      if (project == null)
-      {
-        return HttpNotFound();
-      }
-      return View(project);
-    }
+        // POST: Projects/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Description,Version,DateOfCreate,DateOfUpdate,CompanyId")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CompanyId = new SelectList(db.Company, "Id", "Name", project.CompanyId);
+            return View(project);
+        }
 
-    // POST: Projects/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<ActionResult> DeleteConfirmed(int? id)
-    {
-      //TODO Add null check
-      Project project = await db.Project.FindAsync(id);
-      db.Project.Remove(project);
-      await db.SaveChangesAsync();
-      return RedirectToAction("Index");
-    }
+        // GET: Projects/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = await db.Project.FindAsync(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
 
-    protected override void Dispose(bool disposing)
-    {
-      if (disposing)
-      {
-        db.Dispose();
-      }
-      base.Dispose(disposing);
+        // POST: Projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Project project = await db.Project.FindAsync(id);
+            db.Project.Remove(project);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
-  }
 }
